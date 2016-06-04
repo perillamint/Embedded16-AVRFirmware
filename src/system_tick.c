b18d5c72245f5a8f7cf391d9c503dbe3d14c56ec
perillamint <perillamint@gentoo.moe>
2016-06-04 19:30:57 +0900

    ATMEGA128 support.

--- src/system_tick.c
+++ src/system_tick.c
@@ -7,6 +7,7 @@
 
 void init_system_tick(void)
 {
+#ifndef __AVR_ATmega128__
   //Initialize Timer 0 for scheduler tick.
   OCR0A = (F_CPU / 1024 / SYSTICKS_PER_SEC);
 
@@ -16,12 +17,26 @@ void init_system_tick(void)
   //Timer0 CTC mode, prescaler = 1024
   TCCR0A = _BV(WGM01);
   TCCR0B = _BV(CS00) | _BV(CS02);
+#else
+  //Initialize Timer 0 for scheduler tick.
+  OCR0 = (F_CPU / 1024 / SYSTICKS_PER_SEC);
+
+  //Enable compare-match interrupt.
+  TIMSK = _BV(OCIE0);
+
+  //Timer0 CTC mode, prescaler = 1024
+  TCCR0 = _BV(WGM01) | _BV(CS00) | _BV(CS01) | _BV(CS02);
+#endif
 
   sei();
 }
 
 //Atom scheduler ISR.
+#ifndef __AVR_ATmega128__
 ISR (TIMER0_COMPA_vect)
+#else
+ISR (TIMER0_COMP_vect)
+#endif
 {
   atomIntEnter();
   atomTimerTick();
