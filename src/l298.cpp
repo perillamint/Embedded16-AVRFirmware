057b09b754a22c8f6b5dc4163833254d6737b35e
perillamint <perillamint@gentoo.moe>
2016-06-08 22:47:45 +0900

    fix bug. (use ~ instead of !)

--- src/l298.cpp
+++ src/l298.cpp
@@ -29,7 +29,7 @@ int L298::init(uint32_t freq)
   //Mark our motor direction ports as output
   MOTOR_DIR_PORT_DDR |= _BV(MOT1F_PORT) | _BV(MOT1R_PORT) | _BV(MOT2F_PORT) | _BV(MOT2R_PORT);
   //Turn off all motor dir pins.
-  MOTOR_DIR_PORT &= !(_BV(MOT1F_PORT) | _BV(MOT1R_PORT) | _BV(MOT2F_PORT) | _BV(MOT2R_PORT));
+  MOTOR_DIR_PORT &= ~(_BV(MOT1F_PORT) | _BV(MOT1R_PORT) | _BV(MOT2F_PORT) | _BV(MOT2R_PORT));
   return 0;
 }
 
@@ -45,7 +45,7 @@ int L298::set_motor_dir(uint8_t motor, motor_dir_t dir)
     {
     case 0: //MOT1(F|R)
       //Turn off it first.
-      MOTOR_DIR_PORT &= !(_BV(MOT1F_PORT) | _BV(MOT1R_PORT));
+      MOTOR_DIR_PORT &= ~(_BV(MOT1F_PORT) | _BV(MOT1R_PORT));
 
       if(MOT_FORWARD == dir)
         {
@@ -62,7 +62,7 @@ int L298::set_motor_dir(uint8_t motor, motor_dir_t dir)
       break;
     case 1: //MOT2(F|R)
       //Turn off it first.
-      MOTOR_DIR_PORT &= !(_BV(MOT2F_PORT) | _BV(MOT2R_PORT));
+      MOTOR_DIR_PORT &= ~(_BV(MOT2F_PORT) | _BV(MOT2R_PORT));
 
       if(MOT_FORWARD == dir)
         {
