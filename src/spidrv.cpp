074a3cc8f8a136312ee5717aaad7289795bc986e
perillamint <perillamint@gentoo.moe>
2016-06-13 20:51:30 +0900

    Add SPI ISR.

--- src/spidrv.cpp
+++ src/spidrv.cpp
@@ -18,6 +18,18 @@ static bool is_running = false;
 static uint8_t thread_stack[SPIDRV_STACK_SIZE_BYTES];
 static spimemdata_t spi_mem[MAX_SPIMEM_SIZE];
 
+//Buffers for ISR.
+static uint8_t spi_rx_buf[4];
+static uint8_t spi_tx_buf[4];
+static bool spi_tx_flag, spi_rx_flag;
+
+/**
+ * Brief SPI protocol description:
+ * Packet MUST NOT start with null.
+ * Packet MAY contain null in its data and 4-byte fixed length.
+ * Transmit 0x00 when idling.
+ */
+
 SPIdrv::SPIdrv()
 {
   //
@@ -33,12 +45,16 @@ void SPIdrv::thread_func(uint32_t data)
   //TODO: SPI stuff
   for(;;)
     {
+      //printf_P(PSTR("Hello, SPI!\n"));
       atom_delay_ms(1000);
     }
 }
 
 int SPIdrv::init()
 {
+  DDRB |= _BV(6);
+  SPCR = _BV(SPE) | _BV(SPIE);
+  SPDR = 0x00;
   return 0;
 }
 
@@ -114,4 +130,54 @@ int SPIdrv::read_memory(spimmap_t addr, int16_t *value)
 
   *value = spi_mem[addr].int16;
   return 0;
+}
+
+ISR (SPI_STC_vect)
+{
+  static int idx = 0;
+  static int status = 0; // 0 = rx, 1 = tx.
+
+  switch(status)
+    {
+    case 0: //RX
+      spi_rx_buf[idx] = SPDR;
+      SPDR = 0x00;
+      idx ++;
+      break;
+    case 1:
+      if(spi_tx_flag)
+        {
+          SPDR = spi_tx_buf[idx];
+          idx ++;
+        }
+      else
+        {
+          SPDR = 0x00; //Idle until txflag set.
+        }
+      break;
+    default:
+      printf_P(PSTR("Uh-oh, it shouldn't be here! DEBUG UR SPI_STC_vect.\n"));
+      break;
+    }
+
+  if(idx >= 4)
+    {
+      printf_P(PSTR("SPI mode switch! stat = %d, buf = 0x%X 0x%X 0x%X 0x%X\n"),
+               status, spi_rx_buf[0], spi_rx_buf[1], spi_rx_buf[2], spi_rx_buf[3]);
+      idx = 0;
+
+      switch(status)
+        {
+        case 0:
+          spi_rx_flag = true;
+          break;
+        case 1:
+          spi_tx_flag = false;
+          break;
+        default:
+          break;
+        }
+
+      status = (status + 1) % 2;
+    }
 }
